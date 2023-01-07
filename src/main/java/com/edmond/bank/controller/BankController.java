30385054652a188e248652b53a8859176324cbff
user-edmd <111706719+user-edmd@users.noreply.github.com>
2023-01-07 15:13:17 -0800

    updated BankController and html pages to display an individual user and their accounts

--- src/main/java/com/edmond/bank/controller/BankController.java
+++ src/main/java/com/edmond/bank/controller/BankController.java
@@ -1,13 +1,16 @@
 package com.edmond.bank.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.edmond.bank.dao.AccountRepository;
 import com.edmond.bank.dao.TransactionsRepository;
@@ -21,25 +24,46 @@ import com.edmond.bank.service.UserService;
 
 @Controller
 public class BankController {
-	
+
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private UserRepository userRepository;
+
 	@GetMapping("/")
 	public String home() {
 		return "index";
 	}
-	
+
 	@GetMapping("/edmond")
 	public String edmondsAccount(Model theModel) {
 		User userEdmond = userService.findById(2);
 		theModel.addAttribute("user", userEdmond);
-		theModel.addAttribute("account", userEdmond.getAccountList());
-		theModel.addAttribute("transactions", userEdmond.getAccountList().get(0).getTransactionsList());
-		
+		theModel.addAttribute("account", userEdmond.getAccountList().get(0).getTransactionsList());
+
 		return "edmond";
-		
+
 	}
-	
+
+	@GetMapping("/test")
+	public String testPage(Model theModel) {
+		List<User> userTest = userRepository.findAll();
+		theModel.addAttribute("options", userTest);
+
+		return "test";
+
+	}
+
+	@GetMapping("test/{id}")
+	public String retrieveUser(@PathVariable("id") int id, Model theModel) {
+
+		User userTest = userService.findById(id);
+		theModel.addAttribute("user", userTest);
+		theModel.addAttribute("userAccounts", userTest.getAccountList());
+		theModel.addAttribute("userTransactions", userTest.getAccountList());
+
+		return "testuserpage";
+	}
 
 }
--- src/main/java/com/edmond/bank/dao/TransactionsRepository.java
+++ src/main/java/com/edmond/bank/dao/TransactionsRepository.java
@@ -3,11 +3,11 @@ package com.edmond.bank.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.edmond.bank.entity.Transactions;
 
+@Repository
 public interface TransactionsRepository extends JpaRepository<Transactions, Integer> {
 	
 	List<Transactions> findByAccountId(int accountId);
